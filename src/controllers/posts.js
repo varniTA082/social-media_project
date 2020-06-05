b12f5297f6c77732bd0bc18e5ed0124ae469ed5e
varniTA082 <varnitasharma5@gmail.com>
2020-06-05 17:23:58 +0530

    changes added

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,92 +1,92 @@
-const { Posts, Users, Comments } = require('../models/db')
+// const { Posts, Users, Comments } = require('../models/db')
 
-async function createPosts(title, content, authorId) {
-    if (typeof title !== 'string' || title.length < 1) {
-        throw new Error('Title empty or undefined')
-    }
-    if (typeof content !== 'string' || content.length < 1) {
-        throw new Error('Content empty or undefined')
-    }
-    if (typeof authorId !== 'number') {
-        throw new Error('Author of article not defined')
-    }
-    try {
-        return await Posts.create({
-            title,
-            content,
-            authorId
-        })
-    } catch (e) {
-        throw e
-    }
-}
-
-async function fetchPosts() {
-    try {
-        return await Posts.findAll({
-            include: [
-                { model: Users, as: 'author', attributes: ['username'] }
-            ]
-        })
-    } catch (e) {
-        throw (e)
-    }
-}
-
-async function fetchPostsById(postId) {
-    try {
-        return await Posts.findById(postId, {
-            include: [{
-                model: Comments,
-                include: [{
-                    model: Users,
-                    attributes: ['username']
-                }]
-            }]
-        })
-    } catch (e) {
-        throw e
-    }
-}
-
-module.exports = {
-    createPosts,
-    fetchPosts,
-    fetchPostsById
-}
-
-// const { Posts, Users } = require('../db/models')
-
-// async function createNewPost(userId, title, body) {
-//     const post = await Posts.create({
-//         title,
-//         body,
-//         userId,
-//     })
-
-//     return post
+// async function createPosts(title, content, authorId) {
+//     if (typeof title !== 'string' || title.length < 1) {
+//         throw new Error('Title empty or undefined')
+//     }
+//     if (typeof content !== 'string' || content.length < 1) {
+//         throw new Error('Content empty or undefined')
+//     }
+//     if (typeof authorId !== 'number') {
+//         throw new Error('Author of post not defined')
+//     }
+//     try {
+//         return await Posts.create({
+//             title,
+//             content,
+//             authorId
+//         })
+//     } catch (e) {
+//         throw e
+//     }
 // }
+
+// async function fetchPosts() {
+//     try {
+//         return await Posts.findAll({
+//             include: [
+//                 { model: Users, as: 'author', attributes: ['username'] }
+//             ]
+//         })
+//     } catch (e) {
+//         throw (e)
+//     }
+// }
+
+// async function fetchPostsById(postId) {
+//     try {
+//         return await Posts.findById(postId, {
+//             // include: [{
+//             //     model: Comments,
+//             include: [{
+//                     model: Users,
+//                     attributes: ['username']
+//                 }]
+//                 // }]
+//         })
+//     } catch (e) {
+//         throw e
+//     }
+// }
+
+// module.exports = {
+//     createPosts,
+//     fetchPosts,
+//     fetchPostsById
+// }
+
+const { Posts, Users } = require('../db/models')
+
+async function createNewPost(userId, title, body) {
+    const post = await Posts.create({
+        title,
+        body,
+        userId,
+    })
+
+    return post
+}
 
 // /**
 //  * showAllPosts({username: ''})
 //  * showAllPosts({title: ''})
 //  */
-// async function findAllPosts(query) {
-//     let where = {}
-//     if (query.userId) { where.userId = query.userId }
+async function findAllPosts(query) {
+    let where = {}
+    if (query.userId) { where.userId = query.userId }
 
-//     const posts = await Posts.findAll({
-//         include: [Users],
-//         where
-//     })
+    const posts = await Posts.findAll({
+        include: [Users],
+        where
+    })
 
-//     return posts
-// }
+    return posts
+}
 
-// module.exports = {
-//     createNewPost,
-//     findAllPosts
-// }
+module.exports = {
+    createNewPost,
+    findAllPosts
+}
 
 /* Test Code */
 /*
